9194f4408b5158544a3da8894d535bbf33bed01a
meekeesh <37770594+meekeesh@users.noreply.github.com>
2022-07-06 15:01:25 +0300

    .

--- agency/settings.py
+++ agency/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=gh+1(nl%xua16kb66w4=p074lo_iz*c^#q9o2kwz36#n=z_rs'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
